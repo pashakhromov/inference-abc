c34522061d55b66080588917e95861e5f58135a1
Pasha Khromov <pasha.khromov@gmail.com>
2019-08-24 17:24:49 -0400

    bash -> python

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-bash job2L.sh
-bash job2R.sh
-bash job3L.sh
-bash job3R.sh
-bash jobX.sh
+python job2L.py &
+python job2R.py &
+python job3L.py &
+python job3R.py &
+python jobX.py &
