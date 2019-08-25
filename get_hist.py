cde34d685e27a63ddfaf97d216104221ce6af59f
Pasha Khromov <pasha.khromov@gmail.com>
2019-08-25 11:08:59 -0500

    updated directories

--- get_hist.py
+++ get_hist.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from seqtools import sample2hist
+import os
 
 import time
 t1 = time.time()
@@ -8,14 +9,15 @@ ch = '2L'
 n_itr = 10000
 resample_size = 5
 n_subset = 100
+path = '/home/pasha/chr_data'
 
-sample = pd.read_csv('/scratch/pnk16/abc/data/sample_{}.csv'.format(ch), index_col=0)
+sample = pd.read_csv(os.path.join(path, 'chr_{}_sample.csv'.format(ch)), index_col=0)
 
 if n_subset:
     sample = sample.iloc[:n_subset, :]
 
 hist = sample.apply(lambda x: sample2hist(x, resample_size=resample_size, n_itr=n_itr), axis=1)
-hist.fillna(0).to_csv('hist_{}.csv'.format(ch))
+hist.fillna(0).to_csv(os.path.join(path, 'chr_{}_hist.csv'.format(ch)))
 
 t2 = time.time()
 with open('time.txt', 'w') as f:
