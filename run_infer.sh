9bfdc49a1e9dd8b0b0b7d23bc4e51bc2ab3278bf
Pasha Khromov <pasha.khromov@gmail.com>
2019-08-27 20:58:05 -0500

    split 2R into 2 tasks

--- run_infer.sh
+++ run_infer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 python3 infer_2L.py &
-python3 infer_2R.py &
+python3 infer_2R_1.py &
+python3 infer_2R_2.py &
 python3 infer_3L.py &
 python3 infer_3R.py &
 python3 infer_X.py &
