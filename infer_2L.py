399d17ac5046975dcedcc18b2627b91358eb29bf
Pasha Khromov <pasha.khromov@gmail.com>
2019-08-27 20:19:23 -0500

    now measure time wrt stat_obs and not sim

--- infer_2L.py
+++ infer_2L.py
@@ -11,13 +11,8 @@ n_sub = 1000
 
 path = '/home/pasha/chr_data'
 
-par_sim = pd.read_csv(os.path.join(
-    path, 'par_sim_{}.csv'.format(prior)), index_col=0)
-stat_sim = pd.read_csv(os.path.join(
-    path, 'stat_sim_{}.csv'.format(prior)), index_col=0)
-if n_sub:
-    par_sim = par_sim.iloc[:n_sub, :]
-    stat_sim = stat_sim.iloc[:n_sub, :]
+par_sim = pd.read_csv(os.path.join(path, 'par_sim_{}.csv'.format(prior)), index_col=0)
+stat_sim = pd.read_csv(os.path.join(path, 'stat_sim_{}.csv'.format(prior)), index_col=0)
 
 idx_non_conv = stat_sim[stat_sim.isnull().any(axis=1)].index
 par_sim = par_sim.drop(idx_non_conv, axis=0)
@@ -38,20 +33,27 @@ def get_post_idx(stat_obs):
     return pd.Series(idx)
 
 
-# post_idx = stat_obs.apply(get_post_idx, axis=1)
-# post_idx.to_csv(os.path.join(data_path, 'post_idx_{}.csv'.format(ch)))
-write_header = True
-for stat_obs in pd.read_csv(os.path.join(path, 'chr_{}_hist.csv'.format(ch)), index_col=0, chunksize=1000):
-    stat_obs = stat_obs.astype(float)
-    post_idx = stat_obs.apply(get_post_idx, axis=1)
-    fname = os.path.join(path, 'chr_{}_post_idx.csv'.format(ch))
-    if write_header:
-        post_idx.to_csv(fname, mode='a', header=True)
-        write_header = False
-    else:
-        post_idx.to_csv(fname, mode='a', header=False)
+# #post_idx = stat_obs.apply(get_post_idx, axis=1)
+# #post_idx.to_csv(os.path.join(data_path, 'post_idx_{}.csv'.format(ch)))
+# write_header = True
+# for stat_obs in pd.read_csv(os.path.join(path, 'chr_{}_hist.csv'.format(ch)), index_col=0, chunksize=1000):
+#     stat_obs = stat_obs.astype(float)
+#     post_idx = stat_obs.apply(get_post_idx, axis=1)
+#     fname = os.path.join(path, 'chr_{}_post_idx.csv'.format(ch))
+#     if write_header:
+#         post_idx.to_csv(fname, mode='a', header=True)
+#         write_header = False
+#     else:
+#         post_idx.to_csv(fname, mode='a', header=False)
+
+stat_obs = pd.read_csv(os.path.join(path, 'chr_{}_hist.csv'.format(ch)), index_col=0)
+if n_sub:
+    stat_obs = stat_obs.iloc[:n_sub, :]
+stat_obs = stat_obs.astype(float)
+post_idx = stat_obs.apply(get_post_idx, axis=1)
+post_idx.to_csv(os.path.join(path, 'chr_{}_post_idx.csv'.format(ch)))
 
 t2 = time.time()
 fname = os.path.join(path, 'infer_runtime_{}.txt'.format(ch))
 with open(fname, 'w') as f:
-    f.write('{:0.2}\n'.format(t2-t1))
+    f.write('{:0.2f}\n'.format(t2-t1))
