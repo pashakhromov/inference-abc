2dd8ae8f8a53f4466a89e7dfb7c13bcdae782dbe
Pasha Khromov <pasha.khromov@gmail.com>
2019-08-29 20:10:38 -0500

    run only 3L and 3R

--- run_infer.sh
+++ run_infer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python3 infer_2L.py &
-python3 infer_2R.py &
+# python3 infer_2L.py &
+# python3 infer_2R.py &
 python3 infer_3L_1.py &
 python3 infer_3L_2.py &
-# python3 infer_3R_1.py &
-# python3 infer_3R_2.py &
-python3 infer_X.py &
+python3 infer_3R_1.py &
+python3 infer_3R_2.py &
+# python3 infer_X.py &
