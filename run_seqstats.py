0b151ca817c23623871248234ff56857c2e7fc91
Pasha Khromov <pasha.khromov@gmail.com>
2019-09-25 19:57:31 -0500

    Added pool

--- run_seqstats.py
+++ run_seqstats.py
@@ -1,15 +1,37 @@
+#!/usr/bin/env python3
+
 from seqstats import main
 import sys
 from multiprocessing import Pool
+import pandas as pd
+import os
+import time
+from myio import get_path
 
 if __name__ == '__main__':
+    t1 = time.time()
     ch = sys.argv[1]
 
     n_core = 3
     pool = Pool(n_core)
 
     args = [{'ch': ch, 'chunk': i, 'n_chunk': n_core} for i in range(n_core)]
-
-    # print(args)
-    # results = pool.map(main, zip(a, b))
     results = pool.map(main, args)
+
+    out = {k: [] for k in results[0].keys()}
+
+    for r in results:
+        for k, v in out.items():
+            out[k].append(r[k])
+
+    path = get_path()
+    for k, v in out.items():
+        fpath = os.path.join(
+            path['out'], 'chr_{ch}_{k}.csv'.format(ch=ch, k=k))
+        pd.concat(v).to_csv(fpath)
+
+    t2 = time.time()
+    fpath = os.path.join(
+        path['out'], 'chr_{ch}_runtime_seqstats.csv'.format(ch=ch))
+    with open(fpath, 'w') as f:
+        f.write('{:0.1f}\n'.format(t2-t1))
