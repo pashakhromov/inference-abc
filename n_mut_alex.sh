104a1d254a4c133642ada06bdbac53fc5f158664
pasha <pasha.khromov@gmail.com>
2021-05-26 20:58:52 -0400

    Removed draft files

--- n_mut_alex.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python3 cds_pop.py &
-python3 cds_ref.py &
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python job2L.py &
-python job2R.py &
-python job3L.py &
-python job3R.py &
-python jobX.py &
